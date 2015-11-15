3ea91a973e8bc4dfc4fabe04ccdc5b0719a7e301
Tory Burgett <vdburgett@gmail.com>
2015-11-15 17:49:34 -0500

    Devise and Cancan added to project

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,4 +1,8 @@
 class PostController < ApplicationController
+  load_and_authorize_resource
+  before_action :authenticate_user!
+  before_action :set_status, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
